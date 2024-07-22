fdbf9ca508a63d78d4f53b5a98e400a6fe099426
FilipTLW <filipmyslinski2006@gmail.com>
2024-07-22 10:06:53 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import {CommonModule} from "@angular/common";
+import {RouterLink, RouterOutlet} from "@angular/router";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import {MatListModule} from "@angular/material/list";
+import {MatIconModule} from "@angular/material/icon";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @Component({
+  imports: [CommonModule, RouterOutlet, MatToolbarModule, MatButtonModule, MatListModule, MatIconModule, RouterLink, MatSidenavModule],
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  standalone: true,
+  styleUrls: ['./app.component.scss'],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'WahlsystemUSA';
 }
